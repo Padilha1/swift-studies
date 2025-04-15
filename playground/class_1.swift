b5c1db0f8b3f017fa72715ec0b9db5cf4a0326d0
Padilha1 <padilhauchiha@gmail.com>
2025-04-15 13:28:59 -0300

    Diving into Swift

---
+++ playground/class_1.swift
@@ -0,0 +1,26 @@
+
+struct ContaCorrente {
+    var saldo = 0.0
+    var nome: String
+
+    mutating func sacar(_ valor: Double) {
+        saldo -= valor
+    }
+
+    mutating func depositar (_ valor: Double) {
+        saldo += valor
+    }
+
+}
+
+
+var contaCorrenteMatheus = ContaCorrente(nome: "Matheus")
+print(contaCorrenteMatheus.saldo)
+contaCorrenteMatheus.depositar(3000)
+print(contaCorrenteMatheus.saldo)
+
+var contaCorrenteCaio = ContaCorrente(nome: "Caio")
+print(contaCorrenteCaio.saldo)
+contaCorrenteCaio.depositar(200)
+contaCorrenteCaio.sacar(100)
+print(contaCorrenteCaio.saldo)
