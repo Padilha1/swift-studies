e554c3989efe994fa9580b18b599261ad8888670
Padilha1 <padilhauchiha@gmail.com>
2025-04-15 12:06:50 -0300

    atividade 2

--- functions.swift
+++ functions.swift
@@ -27,16 +27,40 @@ func autenticar (usuario: String?, senha: String?) {
 autenticar (usuario: "Lucas", senha: "123456")  
 
 //optional chaining 
-if let primeiroCaracter = telefone?.first {
-    print(telefone?.first)
-
-}
+// if let primeiroCaracter = telefone?.first {
+//     print(primeiroCaracter)
+// }
 
 // nil coalescing 
 
-print (telefone ?? "Nao ha valor para telefone")
+// print (telefone ?? "Nao ha valor para telefone")
 
 // Atividade
-let numero: String = "10"
-let inteiro = Int(numero)
-print(inteiro + 1)
+// let numero: String = "10"
+// let inteiro = Int(numero)
+// print(inteiro + 1)
+
+//atividade 2
+
+func recebeNome (nome: String?){
+    print(nome ?? "Nome nao informado")
+}
+
+recebeNome(nome: "Lucas")
+recebeNome(nome: nil)
+
+
+func pagarConta(valor: Double?, pessoas: Int?){
+    guard let valor = valor, let pessoas = pessoas 
+    else { print("Informacoes faltando")
+        return
+    }
+    let garcom: Double = 0.10
+    let total = valor + (valor * garcom)
+
+    let valorPorPessoa = total / Double(pessoas)
+    print("Valor total: \(total)")
+}
+
+
+pagarConta(valor:120.0, pessoas: 4)
